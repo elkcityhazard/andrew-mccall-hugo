e389aaf4bfb10e5fa443954e118ea220b4f8474d
Andrew M McCall <andrew.m.mccall@gmail.com>
2024-06-29 09:57:59 -0400

    added multipart form upload post

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-import { ThemeToggle } from "./themeToggle.js"
-import { TypeWriter } from "./typewriter.js"
-
-const toggle = new ThemeToggle('themeToggle')
-const typewriter = new TypeWriter("siteHeader")
-
-
