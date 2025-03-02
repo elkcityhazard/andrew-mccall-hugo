ce92a26174f650db9b0c188e18f6a8ce0ced0d00
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-03-02 09:53:15 -0500

    build

--- themes/megalawnalien/postcss.config.js
+++ themes/megalawnalien/postcss.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    plugins: [
-       
-       require("postcss-import"),
-       require("postcss-nested"),
-       require("postcss-cssnext"),
-       require("postcss-utilities"),
-       require("autoprefixer"),
-       ...(process.env.NODE_ENV === 'production' ? 
-        require('cssnano')({
-          preset: 'default',
-        }) :[]),
-    ],
-   };
-   
+  plugins: [
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("postcss-cssnext"),
+    require("postcss-utilities"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+        ]
+      : []),
+  ],
+};
