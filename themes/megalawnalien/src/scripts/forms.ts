c12e1634096b29675d1262d5b988e3b9f8d66896
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-03-05 20:12:04 -0500

    update to form

--- themes/megalawnalien/src/scripts/forms.ts
+++ themes/megalawnalien/src/scripts/forms.ts
@@ -46,8 +46,10 @@ async handleOnFormData(e:FormDataEvent) {
             const message = formData.get("message")
             const phoneNumber = formData.get("phone_number")
 
+            const baseUrl = process.env.NODE_ENV == "development" ? "http://localhost:8675" : "https://contact.andrew-mccall.com"
 
-            const resp = await fetch("https://contact.andrew-mccall.com/api/v1/andrew-mccall/contact", {
+
+            const resp = await fetch(baseUrl+"/api/v1/andrew-mccall/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
