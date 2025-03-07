8b2aefad2a091a6b00b03f5cb35ce1dd66c0e35f
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-03-06 21:28:14 -0500

    tweak to menu

--- themes/megalawnalien/postcss.config.js
+++ themes/megalawnalien/postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
     require("postcss-nested"),
     require("postcss-cssnext"),
     require("postcss-utilities"),
-    require("autoprefixer"),
     ...(process.env.NODE_ENV === "production"
       ? [
           require("cssnano")({
