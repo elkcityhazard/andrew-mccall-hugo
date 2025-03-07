a16f577adffc24e29e1a43539388a124bac9e1cf
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-03-06 19:35:35 -0500

    try build in dev mode

--- themes/megalawnalien/webpack.config.js
+++ themes/megalawnalien/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "development" : "production",
+  devtool: "source-map",
   watch: process.env.NODE_ENV === "production" ? false : true,
   entry: "./src/scripts/index.ts",
   module: {
