88d3c4f27ccfded3d2c1af52180c2cb4844bb79c
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-03-09 11:52:44 -0400

    webpack back to prod

--- themes/megalawnalien/webpack.config.js
+++ themes/megalawnalien/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   devtool: "source-map",
   watch: process.env.NODE_ENV === "production" ? false : true,
   entry: "./src/scripts/index.ts",
